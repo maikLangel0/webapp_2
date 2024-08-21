ec4182a9382b94d1df84d25d31c955c1c3b43b4f
Mikael Isaksen <mikaellunde2003@hotmail.com>
2024-08-21 17:57:24 +0200

    first commit

---
+++ test/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Idot!");
+        
+//https://code.visualstudio.com/docs/csharp/get-started <-- SET UP NEW PROJECT
